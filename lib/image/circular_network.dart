388bf996b6f0b9b67904c3d98ce6e66340e442ca
arfinity <himanshuhimsurya@gmail.com>
2023-07-29 01:25:29 +0530

    CircularNetworkImage: Add radius field with default 20

--- lib/image/circular_network.dart
+++ lib/image/circular_network.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CircularNetworkImage extends StatelessWidget {
-  const CircularNetworkImage({super.key, required this.imageLink});
+  const CircularNetworkImage(
+      {super.key, required this.imageLink, this.radius = 20.0});
   final String imageLink;
+  final double radius;
   @override
   Widget build(BuildContext context) {
     // Use below to implement Hero animation for profile picture
     //https: //docs.flutter.dev/ui/animations/hero-animations
     return CircleAvatar(
-      radius: 20,
-      backgroundImage: NetworkImage(
+      radius: radius,
+      foregroundImage: NetworkImage(
         imageLink,
       ),
     );
