137dec039386b2db494a12b6e12cb6bfb5e343df
arfinity <himanshuhimsurya@gmail.com>
2023-07-30 00:49:19 +0530

    Fix bug: Incorrect use of ParentDataWidget
    Remove Widget Expanded within Padding

--- lib/pages/profile/components/badge_card.dart
+++ lib/pages/profile/components/badge_card.dart
@@ -10,45 +10,43 @@ class BadgeCard extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 8),
-                child: const Text('Badges'),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    for (int index = 0; index < badges.length; ++index) ...{
-                      SizedBox(
-                        width: 100,
-                        child: Column(
-                          children: [
-                            Image.network(
-                              badges[index].iconLink!,
-                              scale: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 8),
+              child: const Text('Badges'),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  for (int index = 0; index < badges.length; ++index) ...{
+                    SizedBox(
+                      width: 100,
+                      child: Column(
+                        children: [
+                          Image.network(
+                            badges[index].iconLink!,
+                            scale: 4,
+                          ),
+                          Wrap(children: [
+                            Text(
+                              badges[index].displayName!,
+                              softWrap: true,
                             ),
-                            Wrap(children: [
-                              Text(
-                                badges[index].displayName!,
-                                softWrap: true,
-                              ),
-                            ])
-                          ],
-                        ),
+                          ])
+                        ],
                       ),
-                      const SizedBox(width: 10),
-                    }
-                  ],
-                ),
+                    ),
+                    const SizedBox(width: 10),
+                  }
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
