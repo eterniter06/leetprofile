c15ea8f67014938305326e4b6ec864572b58dd2f
arfinity <himanshuhimsurya@gmail.com>
2023-10-01 19:09:52 +0530

    Change language section
    - Add logos for languages
    - Update readme with logo sources
    - Remove individual cards per language
    - Add tooltip for language logo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LeetProfile
 LeetProfile is a flutter application for fetching and storing Leetcode user profiles.
+
+Language logo were sourced based on the information on programming language environments: https://support.leetcode.com/hc/en-us/articles/360011833974-What-are-the-environments-for-the-programming-languages-
+Programming Language icons:
+    - https://www.logo.wine/logo/Racket_%28programming_language%29
+    - https://devicon.dev
--- lib/pages/profile/components/language_card.dart
+++ lib/pages/profile/components/language_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:ui_elements/dataclass/user_class/userdata.dart';
 
 class LanguageCard extends StatelessWidget {
@@ -8,36 +9,65 @@ class LanguageCard extends StatelessWidget {
   });
   final LanguageSubmission languageDetails;
 
+  final Map<String, String> map = const {
+    'bash': 'bash.svg',
+    'c': 'c.svg',
+    'c++': 'cplusplus.svg',
+    'java': 'java.svg',
+    'javascript': 'javascript.svg',
+    'ms sql server': 'mssql.svg',
+    'mysql': 'mysql.svg',
+    'python': 'pythonYellow.svg',
+    'python3': 'python.svg',
+    'typescript': 'typescript.svg',
+    // Languages below haven't been tested as may not be rendered
+    //due to a possible difference in how the language name is stored
+    'c#': 'cshap.svg',
+    'dart': 'dart.svg',
+    'elixir': 'elixir.svg',
+    'erlang': 'erlang.svg',
+    'go': 'go.svg',
+    'kotlin': 'kotlin.svg',
+    'oracle': 'oracle.svg',
+    'php': 'php.svg',
+    'racket': 'racket.svg',
+    'ruby': 'ruby.svg',
+    'scala': 'scala.svg',
+    'swift': 'swift.svg',
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      child: AspectRatio(
-        aspectRatio: 1.1,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Text(
-                    languageDetails.problemsSolved.toString(),
-                  ),
+    return Tooltip(
+      message: languageDetails.languageName!,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2,
+              child:
+                  map.containsKey(languageDetails.languageName!.toLowerCase())
+                      ? SvgPicture.asset(
+                          'assets/languages/${map[languageDetails.languageName!.toLowerCase()]!}',
+                          height: 50,
+                        )
+                      : SizedBox(
+                          height: 50,
+                          child: Text(languageDetails.languageName!),
+                        ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text(
+                  languageDetails.problemsSolved.toString(),
                 ),
               ),
-              Expanded(
-                child: Center(
-                  heightFactor: 1,
-                  child: Text(
-                    languageDetails.languageName!,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
