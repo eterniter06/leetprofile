d819dc990102249f9008c712bbe9cb94ba60459c
arfinity <himanshuhimsurya@gmail.com>
2023-10-12 18:14:44 +0530

    Remove card from badges

--- lib/pages/profile/components/badge_card.dart
+++ lib/pages/profile/components/badge_card.dart
@@ -22,35 +22,30 @@ class BadgeCard extends StatelessWidget implements ClassName {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               for (int index = 0; index < badges.length; ++index) ...{
-                Card(
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width:
-                              min(MediaQuery.of(context).size.width / 4, 160),
-                          child: CachedNetworkImage(
-                            imageUrl: badges[index].iconLink!,
-                            // placeholder: (context, url) => AssetImage(asset: 'asset/'),
-                          ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: min(MediaQuery.of(context).size.width / 4, 160),
+                        child: CachedNetworkImage(
+                          imageUrl: badges[index].iconLink!,
+                          // placeholder: (context, url) => AssetImage(asset: 'asset/'),
                         ),
-                        SizedBox(
-                          width:
-                              min(MediaQuery.of(context).size.width / 4, 160),
-                          height: 60,
-                          child: Center(
-                            child: Wrap(children: [
-                              Text(
-                                badges[index].displayName!,
-                                softWrap: true,
-                              ),
-                            ]),
-                          ),
-                        )
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                        width: min(MediaQuery.of(context).size.width / 4, 160),
+                        height: 60,
+                        child: Center(
+                          child: Wrap(children: [
+                            Text(
+                              badges[index].displayName!,
+                              softWrap: true,
+                            ),
+                          ]),
+                        ),
+                      )
+                    ],
                   ),
                 ),
                 const SizedBox(width: 10),
